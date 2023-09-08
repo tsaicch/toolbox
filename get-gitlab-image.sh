02f3c437ded76626506582c7f5bf5ede35f47459
tsaicch <90241325+tsaicch@users.noreply.github.com>
2023-09-08 11:52:09 +0800

    Update get-gitlab-image.sh

--- get-gitlab-image.sh
+++ get-gitlab-image.sh
@@ -19,6 +19,6 @@ helm search repo gitlab --versions
 # versions is chart version
 #helm template gitlab15-4-6 gitlab/gitlab --version 6.4.6 --set global.hosts.domain=DOMAIN   --set certmanager-issuer.email=me@example.com
 #helm template gitlab15-4-6 gitlab/gitlab --version 6.4.6 --set global.hosts.domain=DOMAIN   --set certmanager-issuer.email=me@example.com|grep image|awk -F: '{print $2":"$3}'|sed 's/"//g'|grep -v IfNotPrese
-helm template gitlab-temp gitlab/gitlab --version $GITLAB_CHART_VERSION --set global.hosts.domain=DOMAIN   --set certmanager-issuer.email=me@example.com|grep image|awk -F: '{print $2":"$3}'|sed 's/"//g'|grep -v IfNotPrese|sed 's/@sha256//g' |sed 's/^[ \t]*//g'
+helm template gitlab-temp gitlab/gitlab --version $GITLAB_CHART_VERSION --set global.hosts.domain=DOMAIN   --set certmanager-issuer.email=me@example.com|grep image|awk -F: '{print $2":"$3}'|sed 's/"//g'|grep -v IfNotPrese|sed 's/@sha256//g' |sed 's/^[ \t]*//g'|tee $GITLAB_VERSION-gitlab-image-list
 
-for i in `cat gitlab-image-15-11-12.list `;do echo "FROM $i" > Dockerfile; mylabbuild tsaicch/$i;done
+for i in `cat $GITLAB_VERSION-gitlab-image-list`;do echo "FROM $i" > Dockerfile; mylabbuild tsaicch/$i;done
