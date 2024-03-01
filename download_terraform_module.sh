a98bdfc87833ffaf77250929b21976bdec6495d1
tsaicch <90241325+tsaicch@users.noreply.github.com>
2024-03-01 14:19:36 +0800

    Create download_terraform_module.sh

---
+++ download_terraform_module.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+## install terraform
+wget -O terraform.zip https://releases.hashicorp.com/terraform/1.7.4/terraform_1.7.4_linux_amd64.zip
+unzip terraform.zip
+cp terraform /usr/local/bin/
+
+## download module
+
+## download provider
