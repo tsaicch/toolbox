c9a5c59e8b6a8a21212d9f9fa1e82f3e1f84991f
tsaicch <90241325+tsaicch@users.noreply.github.com>
2024-08-13 18:57:13 +0800

    Update get-gitlab-image.sh

--- get-gitlab-image.sh
+++ get-gitlab-image.sh
@@ -3,9 +3,10 @@ GITLAB_VERSION=$1 #15.4.6
 #GITLAB_CHART_VERSION=$2 #6.4.6
 
 IFS='.' read -r major minor patch <<< "$GITLAB_VERSION"
+echo $major
 new_major=$(expr $major - 9)
-GITLAB_CHART_VERSION="$new_major.$minor.$path"
-
+GITLAB_CHART_VERSION="$new_major.$minor.$patch"
+echo $GITLAB_CHART_VERSION
 env_init()
 {
   wget -O helm.tar.gz https://get.helm.sh/helm-v3.11.1-linux-amd64.tar.gz
@@ -23,7 +24,7 @@ helm search repo gitlab --versions
 # versions is chart version
 #helm template gitlab15-4-6 gitlab/gitlab --version 6.4.6 --set global.hosts.domain=DOMAIN   --set certmanager-issuer.email=me@example.com
 #helm template gitlab15-4-6 gitlab/gitlab --version 6.4.6 --set global.hosts.domain=DOMAIN   --set certmanager-issuer.email=me@example.com|grep image|awk -F: '{print $2":"$3}'|sed 's/"//g'|grep -v IfNotPrese
-helm template gitlab-temp gitlab/gitlab --version $GITLAB_CHART_VERSION --set global.hosts.domain=DOMAIN   --set certmanager-issuer.email=me@example.com|grep image:|awk -F: '{print $2":"$3}'|sed 's/"//g'|grep -v IfNotPrese|sed 's/@sha256//g' |sed 's/^[ \t]*//g'|tee $GITLAB_VERSION-gitlab-image-list
+helm template gitlab-temp gitlab/gitlab --version $GITLAB_CHART_VERSION --set global.hosts.domain=DOMAIN   --set certmanager-issuer.email=me@example.com|grep image:|awk -F: '{print $2":"$3}'|sed 's/"//g'|grep -v IfNotPrese|sed 's/@sha256//g' |sed 's/^[ \t]*//g'|sort|uniq|tee $GITLAB_VERSION-gitlab-image-list
 
 helm pull gitlab/gitlab --version $GITLAB_CHART_VERSION
 helm search repo gitlab --versions|grep gitlab-runner #helm pull gitlab/gitlab-runner --versions 
