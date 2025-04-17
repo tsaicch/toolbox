9828d412cab9edac5fe86234f43b4d10d7a70dc7
tsaicch <90241325+tsaicch@users.noreply.github.com>
2025-04-17 17:36:15 +0800

    Update custom-vim.sh

--- custom-vim.sh
+++ custom-vim.sh
@@ -3,3 +3,8 @@ echo "set cursorline
 set cursorcolumn
 set hlsearch
 set number" > ~/.vimrc
+
+
+sudo apt install -y bash-completion
+source /usr/share/bash-completion/bash_completion
+source <(kubectl completion bash)
